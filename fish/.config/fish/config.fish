333c82f36c426aa020c5be9562228a8eb3710fc2
ssthakar <ssthakar@ncsu.edu>
2025-01-11 15:44:15 -0500

    added kickstart.nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,16 +5,19 @@ if test -z "$CONTAINER_ID"
     end
     alias ls 'lsd -A'
     set fish_greeting
-    # activate jax python Env
-
+    # python env stuff
     alias gen_dev 'source $HOME/Mlab/Python_stuff/Env/gen_dev/bin/activate.fish && which python'
     alias jax 'source $HOME/Mlab/Python_stuff/Env/JAX/bin/activate.fish && which python'
-    alias vim 'nvim'
     alias activate_py 'source .venv/bin/activate.fish'
+    
+    alias vim 'nvim'
+    
+    # for flex!
     fastfetch
 end
 
 # fish setup for ubuntu containers
 if string match -q "ubuntu*" "$CONTAINER_ID"
   set fish_greeting "Running in an Ubuntu based container"
+  alias simvascular '/usr/local/sv/simvascular/2023-03-27/bin/simvascular'
 end
--- nvim/.config/nvim/lua/custom/init.lua
+++
@@ -1,5 +0,0 @@
--- custom snippets using snipmate
--- vim.g.snipmate_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets/snipmate"
-
--- custom snippets using lua, as god intended
-vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/luasnippets"
